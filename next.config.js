9555d08b78c3b7f86aadc07bb7d145a10e7e7de6
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-21 15:22:16 -0400

    configured webpack/file-loader to handle static assets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+      config.module.rules.push(
+        {
+          test: /\.(mov|mp4|webp|ico|png)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/_next',
+              name: (resourcePath, resourceQuery) => {
+                // Get the file extension
+                const extension = resourcePath.split('.').pop();
+                // Determine the appropriate folder based on the file extension
+                let folder;
+                if (extension === 'webp' || extension === 'ico' || extension === 'png') {
+                  folder = 'static/images';
+                } else {
+                  folder = 'static/videos';
+                }
+                // Generate the new file path
+                const name = `/${folder}/[name].[hash].[ext]`;
+                return name;
+              },
+            },
+          },
+        },
+      );
+      return config;
+    },
+  };
 
 module.exports = nextConfig
