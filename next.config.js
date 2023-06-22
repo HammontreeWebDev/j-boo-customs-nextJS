1013e63b3e23d9970f4c43ba0f84fd5709ff625b
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-22 15:59:52 -0400

    fixed escape

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
     webpack: (config) => {
       config.module.rules.push(
         {
           test: /\.(mov|mp4|webp|ico|png)$/,
           use: {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              publicPath: '/_next',
+              publicPath: "/_next",
               name: (resourcePath, resourceQuery) => {
                 // Get the file extension
-                const extension = resourcePath.split('.').pop();
+                const extension = resourcePath.split(".").pop();
                 // Determine the appropriate folder based on the file extension
                 let folder;
-                if (extension === 'webp' || extension === 'ico' || extension === 'png') {
-                  folder = 'static/images';
+                if (extension === "webp" || extension === "ico" || extension === "png") {
+                  folder = "static/images";
                 } else {
-                  folder = 'static/videos';
+                  folder = "static/videos";
                 }
                 // Generate the new file path
                 const name = `/${folder}/[name].[hash].[ext]`;
