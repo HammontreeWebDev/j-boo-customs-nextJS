bc4fca7c7161a759bdc2ca377f16b10049074808
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-23 15:24:44 -0400

    check to see if component has already loaded when it mounts and update state that way

--- components/CustomVideo.js
+++ components/CustomVideo.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect }from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import useImageLoading from "../utils/imageLoadingUtils";
 
 // components
-const LoadSpinner = dynamic(() => import("./LoadSpinner"), {ssr: false});
+const LoadSpinner = dynamic(() => import("./LoadSpinner"), { ssr: false });
 
 const CustomVideo = (props) => {
     const [isMounted, setIsMounted] = useState(false);
@@ -14,21 +14,28 @@ const CustomVideo = (props) => {
         setIsMounted(true);
     }, []);
 
+    useEffect(() => {
+        // Check if the video has already loaded (e.g., due to cache) when the component mounts
+        if (props.src) {
+            handleImageLoad();
+        }
+    }, [props.src, handleImageLoad]);
+
     const initialClass = isImageLoaded ? "custom-video animate__animated animate__fadeIn" : "hidden";
     const exitClass = "custom-video animate__animated animate__fadeOut";
 
-    
-        const logThis = () => {console.log(isImageLoaded);}
+
+    const logThis = () => { console.log(isImageLoaded); }
 
 
-    if (!isMounted){
+    if (!isMounted) {
         return null
     }
 
     return (
         <>
-        {isImageLoaded ? <p>It is Loaded Baby!</p> : <LoadSpinner />}
-        <video className={props.isPageChanging ? exitClass : initialClass} src={props.src} preload="auto" controls={true} playsInline={true} muted={true} onCanPlay={handleImageLoad} onLoadedData={handleImageLoad} />
+            {isImageLoaded ? null : <LoadSpinner />}
+            <video className={props.isPageChanging ? exitClass : initialClass} src={props.src} preload="auto" controls={true} playsInline={true} muted={true} onLoadedData={handleImageLoad} />
         </>
     )
 }
