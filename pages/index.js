faadf81585d275f77ecb4f2f52f33f1e393bfa8c
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-24 19:19:02 -0400

    moved video to be rendered directly on page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { React } from "react";
+import React, { useState } from "react";
 import useNavigation from "../utils/navigationUtils";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
 // import components
 import GalleryPhoto from "@/components/GalleryPhoto";
@@ -9,34 +9,43 @@ import Header from "@/components/Header";
 import LoadSpinner from "@/components/LoadSpinner";
 
 // Dynamic Imports
-const CustomVideo = dynamic(() => import("../components/CustomVideo"), {
-    loading: () => <LoadSpinner />,
-    ssr: false,
-});
+// const CustomVideo = dynamic(() => import("../components/CustomVideo"), {
+//     loading: () => <LoadSpinner />,
+//     ssr: false,
+// });
 
 // Landing Page | route = /landing
 const Landing = () => {
     // ! keep track of state using helper function to fire exit animations
     const { isPageChanging, handleNavigate } = useNavigation();
+    const [isLoading, setLoading] = useState(true);
 
-    return(
+    function removeLoader() {
+        setLoading(false)
+    }
+
+    const initialClass = "custom-video animate__animated animate__fadeIn";
+    const exitClass = "custom-video animate__animated animate__fadeOut";
+
+    return (
         <>
-        <Header backgroundImage="/img/landing_header.png" isPageChanging={isPageChanging} handleNavigate={handleNavigate} alt="Dirt Bike"/>
-        <PageTitle isPageChanging={isPageChanging} whiteText="&quot;Remembering, Preventing, and Healing:" greenText= "Together Against Strep A&quot;"  />
-        <main>
-            <section className="row">
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-1.png" alt="placeholder image"/>
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-2.png" alt="placeholder image"/>
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-3.png" alt="placeholder image"/>
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-4.png" alt="placeholder image"/>
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-5.png" alt="placeholder image"/>
-                <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-6.png" alt="placeholder image"/>
-            </section>
+            <Header backgroundImage="/img/landing_header.png" isPageChanging={isPageChanging} handleNavigate={handleNavigate} alt="Dirt Bike" />
+            <PageTitle isPageChanging={isPageChanging} whiteText="&quot;Remembering, Preventing, and Healing:" greenText="Together Against Strep A&quot;" />
+            <main>
+                <section className="row">
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-1.png" alt="placeholder image" />
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-2.png" alt="placeholder image" />
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-3.png" alt="placeholder image" />
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-4.png" alt="placeholder image" />
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-5.png" alt="placeholder image" />
+                    <GalleryPhoto isPageChanging={isPageChanging} src="/img/ph-6.png" alt="placeholder image" />
+                </section>
 
-            <section className="row">
-                <CustomVideo isPageChanging={isPageChanging} src={"/vid/ph-vid.mp4"}/>
-            </section>
-        </main>
+                <section className="row">
+                    {isLoading && <LoadSpinner />}
+                    <video className={isPageChanging ? exitClass : initialClass} preload="auto" controls={true} playsInline={true} autoPlay={true} muted={true} onPlay={removeLoader} onLoadedData={removeLoader} style={isLoading ? { visibility: "hidden" } : { visibility: "visible" }} src="/vid/ph-vid.mp4" />
+                </section>
+            </main>
         </>
     )
 };
