9d99ff21bb0b6728a8f208e4bf847834a80d4fb1
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-24 19:07:12 -0400

    simplified loading handler

--- components/CustomVideo.js
+++ components/CustomVideo.js
@@ -1,37 +1,26 @@
-import React, { useEffect, useState } from "react";
-import useImageLoading from "../utils/imageLoadingUtils";
-import dynamic from "next/dynamic";
+import React, { useState } from "react";
+
 
 // components 
-const LoadSpinner = dynamic(() => import("./LoadSpinner"), { ssr: false });
-
-
-
+import LoadSpinner from "./LoadSpinner";
 
 const CustomVideo = (props) => {
-    // keep track of state of image load using helper function
-    const { isImageLoaded, handleImageLoad } = useImageLoading();
-    const [isMounted, setIsMounted] = useState(false);
+    const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    const initialClass = isImageLoaded ?
-        "custom-video animate__animated animate__fadeIn"
-        : "hidden";
-    const exitClass = "custom-video animate__animated animate__fadeOut";
-
-    if (!isMounted) {
-        return null
+    function removeLoader() {
+        setLoading(false)
     }
+
+    const initialClass = isLoading ?
+        "hidden"
+        : "custom-video animate__animated animate__fadeIn";
+    const exitClass = "custom-video animate__animated animate__fadeOut";
 
     return (
         <>
-            {isImageLoaded ? null : <LoadSpinner />}
-
-            <video className={props.isPageChanging ? exitClass : initialClass} preload="auto" controls={true} playsInline={true} muted={true} onCanPlayThrough={handleImageLoad} onLoad={handleImageLoad} onLoadedData={handleImageLoad}>
-                <source src={props.src} type="video/mp4"/>
+        {isLoading && <LoadSpinner />}
+            <video className={props.isPageChanging ? exitClass : initialClass} preload="auto" controls={true} playsInline={true} muted={true} onCanPlayThrough={removeLoader} onLoad={removeLoader} onLoadedData={removeLoader}>
+                <source src={props.src} type="video/mp4" />
             </video>
         </>
     )
