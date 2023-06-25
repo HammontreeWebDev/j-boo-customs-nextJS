4ebd0c5c8b74702622cd8fb4c9a2f2d8b01752a8
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-06-25 14:39:24 -0400

    removed brian from about us section, per client request

--- pages/about_us.js
+++ pages/about_us.js
@@ -45,15 +45,6 @@ const AboutUs = () => {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </>} />
                 </section>
-
-                <section className="row">
-                    <FancyCard classSelector="text-col fancy-text" memberName="Brian Hammontree | Treasurer"
-                        memberPhoto={"/img/brad-placeholder.png"} alt="Brian Hammontree | Treasurer"
-                        aboutText={<>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </>} />
-                </section>
             </main>
         </>
     )
