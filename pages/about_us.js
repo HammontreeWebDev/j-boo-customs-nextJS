d64bf9c966e7c7efc8d4c87ef06e68eeb8393718
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-07-12 13:00:17 -0400

    brad avatar updated - all images should be uploaded now

--- pages/about_us.js
+++ pages/about_us.js
@@ -30,7 +30,7 @@ const AboutUs = () => {
                 {/* About Me Cards */}
                 <section className="row">
                     <FancyCard isPageChanging={isPageChanging} classSelector="text-col fancy-text" memberName="Brad Brown | President"
-                        memberPhoto={"/img/brad-placeholder.png"} alt="Brad Brown | President"
+                        memberPhoto={"/img/brad-avatar4x.webp"} alt="Brad Brown | President"
                         aboutText={<>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
