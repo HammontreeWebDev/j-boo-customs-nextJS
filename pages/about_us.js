d77b1588ccee7cd2550b4f53efe7136034ac35b0
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-07-12 12:50:53 -0400

    added betsy avatar

--- pages/about_us.js
+++ pages/about_us.js
@@ -39,7 +39,7 @@ const AboutUs = () => {
 
                 <section className="row">
                     <FancyCard classSelector="text-col fancy-text" memberName="Betsy Brown | Vice President"
-                        memberPhoto={"/img/brad-placeholder.png"} alt="Betsy Brown | Vice President"
+                        memberPhoto={"/img/betsy-avatar4x.webp"} alt="Betsy Brown | Vice President"
                         aboutText={<>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
