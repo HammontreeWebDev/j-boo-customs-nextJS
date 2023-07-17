51edf60019287b69f7e3ac0fcd542a31710fba3a
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-07-17 15:36:50 -0400

    corrected all z-index issues

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
 
@@ -6,7 +7,7 @@ const Footer = () => {
         <footer className="footer-component">
             <img className="footer-shape" src="/img/footer.svg" alt="Red Arch to enhance design" />
             <p className="copyright">Copyright © 2023 Jesse Ryder Brown Foundation, Inc.</p>
-            <small className="small-text">Website Built By: <span><a className="hyperlink" href="https://www.kailebhammontree.com/" target="_blank" rel="noreferrer">Hammontree Full-Stack Solutions LLC</a></span></small>
+            <small className="small-text">Website Built By: <span><Link id="portfolio-link" className="hyperlink" href="https://www.kailebhammontree.com/" target="_blank" rel="noreferrer">Hammontree Full-Stack Solutions LLC</Link></span></small>
         </footer>
     )
 }
