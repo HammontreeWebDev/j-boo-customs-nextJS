c63a58da552ea89f7ab958ee4e19960cd7153e4e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-07-17 13:40:00 -0400

    fixed crossOrigin issue in next/head, added target _blank for donations link

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,7 @@ const App = ({ Component, pageProps }) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 {/* TODO: import link tag for favicon */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Acme&family=Athiti:wght@400;700&display=swap" rel="stylesheet" />
             </Head>
 
