1086364a3d5ba332a2047b20f951e3a521c14da1
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2023-07-20 22:48:39 -0400

    import google fonts into CSS file directly

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,9 @@ const Document = () => {
     return (
         <Html>
             <Head>
-                {/* TODO: import link tag for favicon */}
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link href="https://fonts.googleapis.com/css2?family=Acme&family=Athiti:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Acme&family=Athiti:wght@400;700&display=swap" rel="stylesheet" /> */}
                 <link rel="icon" href="img/favicon.ico" />
             </Head>
             <body>
