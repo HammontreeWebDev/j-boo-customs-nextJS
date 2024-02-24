a531c1c3ebafe21dca7bb31ccf1c4e0d61daaae6
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2024-02-24 13:47:47 -0500

    added 2024 to copyright information

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return(
         <footer className="footer-component">
             <img className="footer-shape" src="/img/footer.svg" alt="Red Arch to enhance design" />
-            <p className="copyright">Copyright © 2023 Jesse Ryder Brown Foundation, Inc.</p>
+            <p className="copyright">Copyright © 2023 - 2024 Jesse Ryder Brown Foundation, Inc.</p>
             <small className="small-text">Website Built By: <span><Link id="portfolio-link" className="hyperlink" href="https://www.kailebhammontree.com/" target="_blank" rel="noreferrer">Hammontree Full-Stack Solutions LLC</Link></span></small>
         </footer>
     )
